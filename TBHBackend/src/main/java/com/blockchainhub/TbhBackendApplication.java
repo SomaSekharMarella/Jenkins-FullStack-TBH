534b43dc2d5700effb3cc82f0519039b8bf9c393
SomaSekharMarella <2300030411@kluniversity.in>
2025-09-05 15:16:24 +0530

    added jenkins file

--- TBHBackend/src/main/java/com/blockchainhub/TbhBackendApplication.java
+++ TBHBackend/src/main/java/com/blockchainhub/TbhBackendApplication.java
@@ -2,12 +2,18 @@ package com.blockchainhub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TbhBackendApplication {
+public class TbhBackendApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TbhBackendApplication.class, args);
-		System.out.println("Hello TBH :)");
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(TbhBackendApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(TbhBackendApplication.class, args);
+    }
 }
