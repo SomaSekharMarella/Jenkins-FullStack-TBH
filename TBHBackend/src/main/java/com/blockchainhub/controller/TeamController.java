a3172d1ee1770d5cf7416beaeea6f012e3998ebe
SomaSekharMarella <2300030411@kluniversity.in>
2025-09-25 11:33:37 +0530

    added about section

--- TBHBackend/src/main/java/com/blockchainhub/controller/TeamController.java
+++ TBHBackend/src/main/java/com/blockchainhub/controller/TeamController.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/teams")
-@CrossOrigin(origins = "http://localhost:5173") // allow React frontend
+@CrossOrigin(origins = "http://localhost:5173") 
 public class TeamController {
 
     private final TeamService teamService;
--- TBHFrontend/src/components/Navbar.jsx
+++ TBHFrontend/src/components/Navbar.jsx
@@ -1,22 +1,17 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css'; // Import the CSS file
 
 function Navbar() {
   return (
-    <nav style={{
-      background: "#515d8fff",
-      padding: "15px",
-      display: "flex",
-      justifyContent: "center",
-      gap: "20px"
-    }}>
-      <Link to="/" style={{ color: "white", textDecoration: "none" }}>Home</Link>
-      <Link to="/about" style={{ color: "white", textDecoration: "none" }}>About</Link>
-      <Link to="/team" style={{ color: "white", textDecoration: "none" }}>Team</Link>
-      <Link to="/events" style={{ color: "white", textDecoration: "none" }}>Events</Link>
-      <Link to="/contact" style={{ color: "white", textDecoration: "none" }}>Contact Us</Link>
+    <nav className="navbar">
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/team">Team</Link>
+      <Link to="/events">Events</Link>
+      <Link to="/contact">Contact Us</Link>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
